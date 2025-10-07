067b4d6f4936a6994b823835b901d9b82205c6e3
Jaelre <jaelre@gmail.com>
2025-10-07 16:05:50 +0200

    starting: initial commit with API documentation

---
+++ MetricAid API Documentation_files/api_project.js
@@ -0,0 +1,19 @@
+define({
+  "name": "MetricAid API",
+  "version": "2.0.0",
+  "description": "MetricAids Public API Service",
+  "title": "MetricAid API Documentation",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "url": "https://api.metricaid.com/api/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2025-10-06T13:35:42.744Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
