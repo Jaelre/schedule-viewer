e8ae8e7a6e4c8d785679fa5700a50e636af53517
Jaelre <jaelre@gmail.com>
2025-10-08 15:22:28 +0200

    chore: changed page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from './providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Monthly Shift Viewer',
+  title: 'Turni PS Policlinico',
   description: 'Visualizzazione turni mensili',
   icons: {
     icon: [
