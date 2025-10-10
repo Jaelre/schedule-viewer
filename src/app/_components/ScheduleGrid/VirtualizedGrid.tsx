83073d65e47e0d38458b447709a5d4b11bf6afad
Jaelre <jaelre@gmail.com>
2025-10-10 19:27:33 +0200

    Align day grid padding with compact name column

--- src/app/_components/ScheduleGrid/VirtualizedGrid.tsx
+++ src/app/_components/ScheduleGrid/VirtualizedGrid.tsx
@@ -156,7 +156,7 @@ export function VirtualizedGrid({
         <div
           className="days-content-wrapper"
           style={{
-            paddingLeft: nameColumnWidth,
+            paddingLeft: currentNameColumnWidth,
             zIndex: 1,
           }}
         >
