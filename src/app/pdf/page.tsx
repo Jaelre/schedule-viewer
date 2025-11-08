fdbd947a78e45e62b6c91763d7a248fdc9766562
Jaelre <jaelre@gmail.com>
2025-11-08 23:06:54 +0100

    fix: wrap pdf export with runtime config (#52)

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,10 +1,13 @@
 import { PasswordGate } from '@/app/_components/PasswordGate'
+import { RuntimeConfigProvider } from '@/lib/config/runtime-config'
 import { PdfExportApp } from './_components/PdfExportApp'
 
 export default function PdfPage() {
   return (
     <PasswordGate>
-      <PdfExportApp />
+      <RuntimeConfigProvider>
+        <PdfExportApp />
+      </RuntimeConfigProvider>
     </PasswordGate>
   )
 }
